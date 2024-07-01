7acf74fd0d4b76439a01eeea2a15042029143e04
OEM Configuration (temporary user) <d.shalabayev@kazdream.kz>
2024-07-01 14:53:30 +0500

    Args

---
+++ args.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+
+	if len(os.Args) == 1 {
+		fmt.Println("Please give one or more floats.")
+		os.Exit(1)
+	}
+	arguments := os.Args
+	min, _ := strconv.ParseFloat(arguments[1], 64)
+	max, _ := strconv.ParseFloat(arguments[1], 64)
+	fmt.Println(min, max)
+}
--- go.mod
+++ go.mod
@@ -1,5 +1,5 @@
 module github.com/dauren10/go-profi
 
-go 1.22.1
+go 1.23
 
 require github.com/mactsouk/go v0.0.0-20180603081621-6a282087f7bd // indirect
